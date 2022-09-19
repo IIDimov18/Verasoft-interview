22bf211f65c3874b3a03f69c74c7b52db7f97308
Ivan Dimov <iidimov18@codingburgas.bg>
2022-09-19 22:55:19 +0300

    App Finished

--- app/src/components/Orders.tsx
+++ app/src/components/Orders.tsx
@@ -2,13 +2,6 @@ import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { fetchOrderRequest } from "../dashboard/actions";
 import { RootState } from "../dashboard/reducers/rootReducer";
-import Tabs from "./orders/Tabs";
-import OrdersList from "./orders/OrdersList";
-import GeneralInfo from "./dashboard/GeneralInfo";
-import PersonalInfo from "./dashboard/PersonalInfo";
-import Activity from "./dashboard/Activity";
-import SmsCarrier from "./dashboard/SmsCarrier";
-import BouncingDotsLoader from "./orders/BouncingDotsLoader";
 import ErrorTab from "./orders/ErrorTab";
 
 
@@ -22,34 +15,34 @@ const Orders = () => {
     useEffect(() => {
         dispatch(fetchOrderRequest());
     }, []);
-    const [selectedOrder,setSelectedOrder] = React.useState(orders.orders_AAA)
     const [tabs,setOrderTabs]=React.useState(["inactive","inactive","active","inactive","inactive"]);
     const[errorTab , setErrorTab] = React.useState(false);
     const changeTab= (index:number) =>{
         let response:Array<string> = Array(5).fill('inactive');
-        response[index] = 'active';
+        if (index!=-1){
+                     response[index] = 'active'
+            setErrorTab(false);
+        }
+
         setOrderTabs(response);
 
-        switch(index){
-            case 0:
-                setSelectedOrder(orders.orders_A)
-                break;
-            case 1:
-                setSelectedOrder(orders.orders_AA)
-                break;
-            case 2:
-                setSelectedOrder(orders.orders_AAA)
-                break;
-            case 3:
-                setSelectedOrder(orders.orders_B)
-                break;
-            case 4:
-                setSelectedOrder(orders.orders_C)
-                break;
-        }
+
     }
-    // console.log(orders.orders_AAA);
-    console.log(selectedOrder);
+    function formatDate(orderDate:string){
+        let date = new Date(orderDate);
+        let day = date.toLocaleDateString('default',{weekday: "short"});
+        let month = date.toLocaleDateString('default',{month: "short"});
+        return day+', '+month+" "+date.getDay();
+    }
+    function formatTime(orderTime:string){
+        let time= new Date()
+        let timeSeparated=orderTime.split(':');
+        time.setHours(+timeSeparated[0]);
+        time.setMinutes(+timeSeparated[1]);
+        time.setSeconds(+timeSeparated[2]);
+        let timeFinal = time.toLocaleDateString('default',{hour: 'numeric',minute:'numeric',hour12:true}).split(' ');
+        return timeFinal[1]+' '+timeFinal[2];
+    }
 
     return (
 
@@ -78,78 +71,187 @@ const Orders = () => {
             </div>
             <hr className="orders-hr"/>
             <div className="order-header">
-                {/*<div className="sent">*/}
                 <button className={`sent ${!errorTab?"orders-active" : ""}`} onClick={() =>setErrorTab(false)}>SENT</button>
-                {/*</div>*/}
-                {/*<div className="errors">*/}
-                <button className={`errors ${errorTab?"orders-active" : ""}`} onClick={() =>setErrorTab(true)}>ERRORS</button>
-                {/*</div>*/}
+
+                <button className={`errors ${errorTab?"orders-active" : ""}`} onClick={() =>{setErrorTab(true);changeTab(-1);}}>ERRORS</button>
                 <text className="order-header-text">RECENT ORDERS</text>
             </div>
 
-            <div className="order-description">
-                <text id="order-description-data">DATE & TIME</text>
-                <text id="order-description-subject">SUBJECT</text>
-                <text id="order-description-communication">COMMUNICATION TYPE</text>
-                <text id="order-description-order">ORDER #</text>
 
-            </div>
-            <hr className="order-description-hr"/>
-            <div className="order">
-                <div className="order-date">
-                    <text >Sat, Apr 14</text>
-                    <text>4:19 PM</text>
-                </div>
-                <div className="order-subject">
-                    <text >Thank You Bonus</text>
-                    <text>joe.smith@testemail.com</text>
-                </div>
-                <div className="order-communication">
-                    <text>Promotion Email</text>
-                </div>
-                <div className="order-number">
-                    <text>12345546</text>
-                </div>
-                <button className="resend">RESEND</button>
-                <div>
-                </div>
-            </div>
-            <hr className="order-list-hr"/>
-            {/*<BouncingDotsLoader/>*/}
-            {selectedOrder.sent.map((value =>{
-                return (<div className="order">
-                    <div className="order-date">
-                        <text >Sat, Apr 14</text>
-                        <text>4:19 PM</text>
-                    </div>
-                    <div className="order-subject">
-                        <text >Thank You Bonus</text>
-                        <text>joe.smith@testemail.com</text>
-                    </div>
-                    <div className="order-communication">
-                        <text>Promotion Email</text>
-                    </div>
-                    <div className="order-number">
-                        <text>12345546</text>
-                    </div>
-                    <button className="resend">RESEND</button>
-                </div>
-            )}))}
+            {errorTab?<ErrorTab  />: tabs[0]=='inactive'?null:
+                orders.orders_A.sent!=undefined?
+                    orders.orders_A.sent.map(value => {
+                        return(
+                            <>
+                                <div className="order-description">
+                                    <text id="order-description-data">DATE & TIME</text>
+                                    <text id="order-description-subject">SUBJECT</text>
+                                    <text id="order-description-communication">COMMUNICATION TYPE</text>
+                                    <text id="order-description-order">ORDER #</text>
+                                    <hr className="order-description-hr"/>
+
+                                </div>
+                                <div className={`order ${value.id%2==0?'order-dark':''}`}>
+                                    <div className="order-date">
+                                        <text >{formatDate(value.sent_dt)}</text>
+                                        <text>{formatTime(value.sent_tm)}</text>
+                                    </div>
+                                    <div className="order-subject">
+                                        <text >{value.subject.title}</text>
+                                        <text>{value.subject.email}</text>
+                                    </div>
+                                    <div className="order-communication">
+                                        <text>{value.type}</text>
+                                    </div>
+                                    <div className="order-number">
+                                        <text>{value.order_id}</text>
+                                    </div>
+                                    <button className="resend">RESEND</button>
+                                </div>
+                                <hr className="order-list-hr"/>
+                            </>
+                        )})
+                    : <div className="ErrorTab"><div className="no-item"><text>No Items</text></div></div>}
+                {tabs[1]=='inactive'?null:
+                    orders.orders_AA.sent!=undefined?
+                        orders.orders_AA.sent.map(value => {
+                            return(
+                                <>
+                                    {value.id==1?<div className="order-description">
+                                        <text id="order-description-data">DATE & TIME</text>
+                                        <text id="order-description-subject">SUBJECT</text>
+                                        <text id="order-description-communication">COMMUNICATION TYPE</text>
+                                        <text id="order-description-order">ORDER #</text>
+                                        <hr className="order-description-hr"/>
+
+                                    </div>:null}
+                                    <div className={` order ${value.id%2==0?'order-dark':''}`}>
+                                        <div className="order-date">
+                                            <text >{formatDate(value.sent_dt)}</text>
+                                            <text>{formatTime(value.sent_tm)}</text>
+                                        </div>
+                                        <div className="order-subject">
+                                            <text >{value.subject.title}</text>
+                                            <text>{value.subject.email}</text>
+                                        </div>
+                                        <div className="order-communication">
+                                            <text>{value.type}</text>
+                                        </div>
+                                        <div className="order-number">
+                                            <text>{value.order_id}</text>
+                                        </div>
+                                        <button className="resend">RESEND</button>
+                                    </div>
+                                    <hr className="order-list-hr"/>
+                                </>
+                            )})
+                        : <div className="ErrorTab"><div className="no-item"><text>No Items</text></div></div>}
+                {tabs[2]=='inactive'?null:
+                    orders.orders_AAA.sent!=undefined?
+                        orders.orders_AAA.sent.map(value => {
+                            return(
+                                <>
+                                    {value.id==1?<div className="order-description">
+                                        <text id="order-description-data">DATE & TIME</text>
+                                        <text id="order-description-subject">SUBJECT</text>
+                                        <text id="order-description-communication">COMMUNICATION TYPE</text>
+                                        <text id="order-description-order">ORDER #</text>
+                                        <hr className="order-description-hr"/>
+
+                                    </div>:null}
+                                    <div className={` order ${value.id%2==0?'order-dark':''}`}>
+                                        <div className="order-date">
+                                            <text >{formatDate(value.sent_dt)}</text>
+                                            <text>{formatTime(value.sent_tm)}</text>
+                                        </div>
+                                        <div className="order-subject">
+                                            <text >{value.subject.title}</text>
+                                            <text>{value.subject.email}</text>
+                                        </div>
+                                        <div className="order-communication">
+                                            <text>{value.type}</text>
+                                        </div>
+                                        <div className="order-number">
+                                            <text>{value.order_id}</text>
+                                        </div>
+                                        <button className="resend">RESEND</button>
+                                    </div>
+                                    <hr className="order-list-hr"/>
+                                </>
+                            )})
+                        : <div className="ErrorTab"><div className="no-item"><text>No Items</text></div></div>}
+                {tabs[3]=='inactive'?null:
+                    orders.orders_B.sent!=undefined?
+                        orders.orders_B.sent.map(value => {
+                            return(
+                                <>
+                                    {value.id==1?<div className="order-description">
+                                        <text id="order-description-data">DATE & TIME</text>
+                                        <text id="order-description-subject">SUBJECT</text>
+                                        <text id="order-description-communication">COMMUNICATION TYPE</text>
+                                        <text id="order-description-order">ORDER #</text>
+                                        <hr className="order-description-hr"/>
+
+                                    </div>:null}
+                                    <div className={`order ${value.id%2==0?'order-dark':''}`}>
+                                        <div className="order-date">
+                                            <text >{formatDate(value.sent_dt)}</text>
+                                            <text>{formatTime(value.sent_tm)}</text>
+                                        </div>
+                                        <div className="order-subject">
+                                            <text >{value.subject.title}</text>
+                                            <text>{value.subject.email}</text>
+                                        </div>
+                                        <div className="order-communication">
+                                            <text>{value.type}</text>
+                                        </div>
+                                        <div className="order-number">
+                                            <text>{value.order_id}</text>
+                                        </div>
+                                        <button className="resend">RESEND</button>
+                                    </div>
+                                    <hr className="order-list-hr"/>
+                                </>
+                            )})
+                        : <div className="ErrorTab"><div className="no-item"><text>No Items</text></div></div>}
+                {tabs[4]=='inactive'?null:
+                    orders.orders_C.sent!=undefined?
+
+                        orders.orders_C.sent.map(value => {
+                            return(
+                                <>
+                                    {value.id==1?<div className="order-description">
+                                        <text id="order-description-data">DATE & TIME</text>
+                                        <text id="order-description-subject">SUBJECT</text>
+                                        <text id="order-description-communication">COMMUNICATION TYPE</text>
+                                        <text id="order-description-order">ORDER #</text>
+                                        <hr className="order-description-hr"/>
+
+                                    </div>:null}
+
+                                    <div className={`order ${value.id%2==0?'order-dark':''}`}>
+                                        <div className="order-date">
+                                            <text >{formatDate(value.sent_dt)}</text>
+                                            <text>{formatTime(value.sent_tm)}</text>
+                                        </div>
+                                        <div className="order-subject">
+                                            <text >{value.subject.title}</text>
+                                            <text>{value.subject.email}</text>
+                                        </div>
+                                        <div className="order-communication">
+                                            <text>{value.type}</text>
+                                        </div>
+                                        <div className="order-number">
+                                            <text>{value.order_id}</text>
+                                        </div>
+                                        <button className="resend">RESEND</button>
+                                    </div>
+                                    <hr className="order-list-hr"/>
+                                </>
+                            )})
+                        : <div className="ErrorTab"><div className="no-item"><text>No Items</text></div></div>}
 
 
-
-            {/*{errorTab?<ErrorTab />: selectedOrder.sent.length > 0 ?<OrdersList order={selectedOrder}/> :*/}
-            {/*    <div className="no-item"><text>No Items</text></div>*/}
-            {/*}*/}
-
-            {typeof orders !="undefined" ?(
-                <div className="dashboard">
-
-                </div>
-
-
-
-            ) :<div></div> }
         </>
         </div>
     )
--- app/src/components/orders/ErrorTab.tsx
+++ app/src/components/orders/ErrorTab.tsx
@@ -1,6 +1,5 @@
 import BouncingDotsLoader from "./BouncingDotsLoader";
 import React, {useEffect} from "react";
-import OrdersList from "./OrdersList";
 
 const ErrorTab = () => {
     const [loading,setLoading] = React.useState(true);
