45bd87d00501cc29e157719326ba99f49c3da6ec
Ivan Dimov <iidimov18@codingburgas.bg>
2022-10-06 18:54:38 +0300

    Reorganized components

--- app/src/components/Orders.tsx
+++ app/src/components/Orders.tsx
@@ -1,27 +1,29 @@
 import React, {useEffect} from 'react';
-import { useDispatch, useSelector } from "react-redux";
-import { fetchOrderRequest } from "../dashboard/actions";
-import { RootState } from "../dashboard/reducers/rootReducer";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchOrderRequest} from "../dashboard/actions";
+import {RootState} from "../dashboard/reducers/rootReducer";
 import OrdersList from "./orders/OrdersList"
 import ErrorTab from "./orders/ErrorTab";
 
 
 const Orders = () => {
     const dispatch = useDispatch();
-    const {pending, orders } = useSelector(
+    const {pending, orders} = useSelector(
         (state: RootState) => state.order
     );
     useEffect(() => {
         dispatch(fetchOrderRequest());
     }, []);
-    const[index,setIndex] = React.useState(2);
-    const [tabs,setOrderTabs]=React.useState(["inactive","inactive","active","inactive","inactive"]);
-    const[errorTab , setErrorTab] = React.useState(false);
-    const changeTab= (index:number) =>{
-        let response:Array<string> = Array(5).fill('inactive');
-        if (index!=-1){
-                     response[index] = 'active'
-                    setIndex(index);
+
+    const [errorTab, setErrorTab] = React.useState(false);
+    const [flag, setFlag] = React.useState(false);
+    const [index, setIndex] = React.useState(0);
+    const [tabs, setOrderTabs] = React.useState(["active", "inactive", "inactive", "inactive", "inactive"]);
+    const changeTab = (index: number) => {
+        let response: Array<string> = Array(5).fill('inactive');
+        if (index != -1) {
+            response[index] = 'active'
+            setIndex(index);
             setErrorTab(false);
         }
 
@@ -30,38 +32,36 @@ const Orders = () => {
 
     }
 
-
+    if (orders[index] != undefined && !flag) {
+        orders.map((order) => {
+            if (order.sent != undefined) {
+                setIndex(order.id);
+                changeTab(order.id);
+                setFlag(true);
+            }
+        })
+    }
     return (
 
         <div>
             <>
-            <div className="tabs-container">
-                <div className="tabs">
-                    {orders!=undefined?orders.map((order) =>{
+                <div className="tabs-container">
+                    <div className="tabs">
+                        {orders != undefined ? orders.map((order) => {
 
-                        return(
-                            <div className={`tab ${tabs[order.id]}`} onClick={() =>changeTab(order.id)} >
-                                <text>{order.key.replace("_"," ").toUpperCase()}</text>
-                            </div>
-                        )
+                            return (
+                                <div className={`tab ${tabs[order.id]}`} onClick={() => changeTab(order.id)}>
+                                    <text>{order.key.replace("_", " ").toUpperCase()}</text>
+                                </div>
+                            )
 
-                    }):""}
+                        }) : ""}
+                    </div>
                 </div>
-            </div>
-            <hr className="orders-hr"/>
-            <div className="order-header">
-                <button className={`sent ${!errorTab?"orders-active" : ""}`} onClick={() =>setErrorTab(false)}>SENT</button>
 
-                <button className={`errors ${errorTab?"orders-active" : ""}`} onClick={() =>{setErrorTab(true);}}>ERRORS</button>
-                <text className="order-header-text">RECENT ORDERS</text>
-            </div>
-            {errorTab?<ErrorTab/>:
-            orders[index]!=undefined?orders[index].sent!=undefined?orders[index].sent.map((order) =>{
-                return(
-                    <OrdersList order={order}/>
-                )
-            }):<div className="ErrorTab"><div className="no-item"><text>No Items</text></div></div>:''}
-        </>
+                <hr className="orders-hr"/>
+                <OrdersList orders={orders} index={index}/>
+            </>
         </div>
     )
 }
--- app/src/components/orders/OrdersList.tsx
+++ app/src/components/orders/OrdersList.tsx
@@ -1,58 +1,92 @@
-import {IOrder} from "../../dashboard/types";
+import {IOrder, IOrderModifier} from "../../dashboard/types";
 import React from "react";
+import ErrorTab from "./ErrorTab";
 
-type OrderType={
-    order: IOrder
+type OrderType = {
+    orders: IOrderModifier[],
+    index: number
 }
 
-const OrdersList = (order:OrderType) => {
-    function formatDate(orderDate:string){
+const OrdersList = ({orders, index}: OrderType) => {
+    function formatDate(orderDate: string) {
         let date = new Date(orderDate);
-        let day = date.toLocaleDateString('default',{weekday: "short"});
-        let month = date.toLocaleDateString('default',{month: "short"});
-        return day+', '+month+" "+date.getDay();
+        let day = date.toLocaleDateString('default', {weekday: "short"});
+        let month = date.toLocaleDateString('default', {month: "short"});
+        return day + ', ' + month + " " + date.getDay();
     }
-    function formatTime(orderTime:string){
-        let time= new Date()
-        let timeSeparated=orderTime.split(':');
+
+    function formatTime(orderTime: string) {
+        let time = new Date()
+        let timeSeparated = orderTime.split(':');
         time.setHours(+timeSeparated[0]);
         time.setMinutes(+timeSeparated[1]);
         time.setSeconds(+timeSeparated[2]);
-        let timeFinal = time.toLocaleDateString('default',{hour: 'numeric',minute:'numeric',hour12:true}).split(' ');
-        return timeFinal[1]+' '+timeFinal[2];
+        let timeFinal = time.toLocaleDateString('default', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        }).split(' ');
+        return timeFinal[1] + ' ' + timeFinal[2];
     }
 
+    const [errorTab, setErrorTab] = React.useState(false);
     return (
         <div>
             <>
-                {order.order.id==1?
-                <div className="order-description">
-                    <text id="order-description-data">DATE & TIME</text>
-                    <text id="order-description-subject">SUBJECT</text>
-                    <text id="order-description-communication">COMMUNICATION TYPE</text>
-                    <text id="order-description-order">ORDER #</text>
-                    <hr className="order-description-hr"/>
+                <div className="order-header">
+                    <button className={`sent ${!errorTab ? "orders-active" : ""}`}
+                            onClick={() => setErrorTab(false)}>SENT
+                    </button>
 
+                    <button className={`errors ${errorTab ? "orders-active" : ""}`} onClick={() => {
+                        setErrorTab(true);
+                    }}>ERRORS
+                    </button>
+                    <text className="order-header-text">RECENT ORDERS</text>
                 </div>
-                    :""}
-                <div className={`order ${order.order.id%2==0?'order-dark':''}`}>
-                    <div className="order-date">
-                        <text >{formatDate(order.order.sent_dt)}</text>
-                        <text>{formatTime(order.order.sent_tm)}</text>
-                    </div>
-                    <div className="order-subject">
-                        <text >{order.order.subject.title}</text>
-                        <text>{order.order.subject.email}</text>
-                    </div>
-                    <div className="order-communication">
-                        <text>{order.order.type}</text>
-                    </div>
-                    <div className="order-number">
-                        <text>{order.order.order_id}</text>
-                    </div>
-                    <button className="resend">RESEND</button>
-                </div>
-                <hr className="order-list-hr"/>
+
+                {errorTab ? <ErrorTab/> :
+                    orders[index] != undefined ? orders[index].sent != undefined ? orders[index].sent.map((order) => {
+                        return (
+                            <>
+                                {order.id == 1 ?
+                                    <div className="order-description">
+                                        <div className="order-description-container">
+                                            <text id="order-description-data">DATE & TIME</text>
+                                            <text id="order-description-subject">SUBJECT</text>
+                                            <text id="order-description-communication">COMMUNICATION TYPE</text>
+                                            <text id="order-description-order">ORDER #</text>
+                                        </div>
+                                        <hr className="order-description-hr"/>
+                                    </div>
+                                    : ""}
+                                <div className={`order ${order.id % 2 == 0 ? 'order-dark' : ''}`}>
+                                    <div className="order-date">
+                                        <text>{formatDate(order.sent_dt)}</text>
+                                        <text>{formatTime(order.sent_tm)}</text>
+                                    </div>
+                                    <div className="order-subject">
+                                        <text>{order.subject.title}</text>
+                                        <text>{order.subject.email}</text>
+                                    </div>
+                                    <div className="order-communication">
+                                        <text>{order.type}</text>
+                                    </div>
+                                    <div className="order-number">
+                                        <text>{order.order_id}</text>
+                                    </div>
+                                    <button className="resend">RESEND</button>
+                                </div>
+                                <hr className="order-list-hr"/>
+                            </>
+                        )
+                    }) : <div className="ErrorTab">
+                        <div className="no-item">
+                            <text>No Items</text>
+                        </div>
+                    </div> : ''}
+
+
             </>
         </div>
     )
